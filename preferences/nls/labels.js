92cde60c959c2ac1cc13ac85c7f1e0668e1931fd
parente <parente@3e0aeeee-7800-4400-8353-9f36ae65947a>
2009-04-19 17:18:40 +0000

    * Added missing preference labels
    git-svn-id: http://svn.mindtrove.info/spaceship@28 3e0aeeee-7800-4400-8353-9f36ae65947a

---
+++ preferences/nls/labels.js
@@ -0,0 +1,10 @@
+{
+    SPEECH_VOLUME_LABEL : 'Speech volume',
+    SOUND_VOLUME_LABEL : 'Sound volume',
+    MUSIC_VOLUME_LABEL : 'Music volume',
+    SPEECH_RATE_LABEL : 'Speech rate',
+    MOUSE_CONTROL_LABEL : 'Mouse control',
+    PERCENT_UNITS: '${0}%',
+    SPEECH_RATE_UNITS: '${0} words per minute',
+    BOOLEAN_TYPE_LABELS: ['no', 'yes']
+}
