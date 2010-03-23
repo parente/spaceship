0c047bdef7eca377396a6fd692766abc72ecb526
Peter Parente <parente@cs.unc.edu>
2010-03-22 22:06:45 -0400

    Fixed minigame sound problems
    * Fixed bad stop() call in MiniGame.play() method
    * Removed Match It game sound extensions

--- minigame/matchit/nls/solfege.js
+++ minigame/matchit/nls/solfege.js
@@ -1,13 +1,13 @@
 {
     LISTEN_PROMPT : 'Listen to these notes.',
     CHOICES : [
-        {visual : 'do', sound : '${sounds}solfege/44929__digifishmusic__Solfege_Do.mp3'},
-        {visual : 're', sound : '${sounds}solfege/44934__digifishmusic__Solfege_Re.mp3'},
-        {visual : 'mi', sound : '${sounds}solfege/44933__digifishmusic__Solfege_Me.mp3'},
-        {visual : 'fa', sound : '${sounds}solfege/44931_digifishmusic_Solfege_Fa.mp3'},
-        {visual : 'sol', sound : '${sounds}solfege/44935__digifishmusic__Solfege_So.mp3'},
-        {visual : 'la', sound : '${sounds}solfege/44932__digifishmusic__Solfege_La.mp3'},
-        {visual : 'ti', sound : '${sounds}solfege/44936_digifishmusic_Solfege_Ti.mp3'},
-        {visual : 'do!', sound : '${sounds}solfege/44930__digifishmusic__Solfege_Do_2.mp3'},
+        {visual : 'do', sound : '${sounds}solfege/44929__digifishmusic__Solfege_Do'},
+        {visual : 're', sound : '${sounds}solfege/44934__digifishmusic__Solfege_Re'},
+        {visual : 'mi', sound : '${sounds}solfege/44933__digifishmusic__Solfege_Me'},
+        {visual : 'fa', sound : '${sounds}solfege/44931_digifishmusic_Solfege_Fa'},
+        {visual : 'sol', sound : '${sounds}solfege/44935__digifishmusic__Solfege_So'},
+        {visual : 'la', sound : '${sounds}solfege/44932__digifishmusic__Solfege_La'},
+        {visual : 'ti', sound : '${sounds}solfege/44936_digifishmusic_Solfege_Ti'},
+        {visual : 'do!', sound : '${sounds}solfege/44930__digifishmusic__Solfege_Do_2'},
     ]
 }
